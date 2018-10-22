99d860b45fbbc9058117b4bb81296a53e5a33659
Aarti626 <43464568+Aarti626@users.noreply.github.com>
2018-10-22 16:42:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import json
 import os
 from flask import Flask, render_template, request, Response, stream_with_context, jsonify,flash,Markup
 from os import listdir, rename, remove
-from os.path import dirname, isfile, join
+
 
 from pymongo import MongoClient
 connection = MongoClient()
@@ -13,17 +13,15 @@ collection = db.candidate
 
 app = Flask(__name__)
 
-directory="./candidate"
-for filename in os.listdir(directory):
-    if filename.endswith(".json"):
-        x=os.path.join(directory,filename)
-        print x
-        y=open(x,"r").read()
-        e=json.loads(y)
-	#print e
-	#z=json.loads(e)
-	#print z
-        collection.insert(e)
+
+x="a1037.json"
+print x
+y=open(x,"r").read()
+e=json.loads(y)
+#print e
+#z=json.loads(e)
+#print z
+collection.insert(e)
        
             
       
